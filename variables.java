93c6fe86235cfec8ba46b6f736ebba67b28f66bd
Akshatha MK <akshatha.mk.12.04@gmail.com>
2023-03-01 22:47:18 +0530

    Learning Variables in Java

---
+++ variables.java
@@ -0,0 +1,16 @@
+
+
+
+public class variables {
+    public static void main(String a[])
+    {
+       /* int num = 3;
+        int num2 = 4;
+        System.out.println(num+num2);
+        */
+        int num = 3;
+        int num2 = 4;
+        int res = num+num2;
+        System.out.println(res);
+    }
+}
